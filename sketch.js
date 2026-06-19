122c7aa3d68ba213c7502320a89eafa12cdd0884
moqalatte <catalina.soto5@mail.udp.cl>
2026-06-19 15:20:46 -0400

    Subir mi primer codigo

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(78,150,193);
+  line(50,50,50,300);
+  line(50,50,300,50);
+  line(300,300,300,50);
+  line(300,300,50,300);
+  ellipse(175,175,200,200);
+  square(0,0,50);
+  square(0,300,50)
+  square(300,400,50)
+}
